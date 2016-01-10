c47977f01fabb5f8a24ebc457a89a3dde0fedda2
talmeym <mtalmey@gmail.com>
2016-01-10 14:15:56 +0000

    allowed http call and isolate steps to hold multiple children

--- src/test/java/com/emarte/regurgitator/test/IsolateXmlLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/IsolateXmlLoaderTest.java
@@ -10,16 +10,16 @@ public class IsolateXmlLoaderTest extends XmlLoaderTest {
 
 	@Test
 	public void testMin() throws Exception {
-		assertExpectation("classpath:/Isolate_min.xml", "com.emarte.regurgitator.core.Isolate:['isolate-1',com.emarte.regurgitator.test.stuff.TestStep:['test-step-1'],false,false]");
+		assertExpectation("classpath:/Isolate_min.xml", "com.emarte.regurgitator.core.Isolate:['isolate-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1'], com.emarte.regurgitator.test.stuff.TestStep:['test-step-2']],false,false]");
 	}
 
 	@Test
 	public void testSession() throws Exception {
-		assertExpectation("classpath:/Isolate_session.xml", "com.emarte.regurgitator.core.Isolate:['isolate-1',com.emarte.regurgitator.test.stuff.TestStep:['test-step-1'],true,false]");
+		assertExpectation("classpath:/Isolate_session.xml", "com.emarte.regurgitator.core.Isolate:['isolate-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],true,false]");
 	}
 
 	@Test
 	public void testParameters() throws Exception {
-		assertExpectation("classpath:/Isolate_parameters.xml", "com.emarte.regurgitator.core.Isolate:['isolate-1',com.emarte.regurgitator.test.stuff.TestStep:['test-step-1'],false,true]");
+		assertExpectation("classpath:/Isolate_parameters.xml", "com.emarte.regurgitator.core.Isolate:['isolate-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],false,true]");
 	}
 }
