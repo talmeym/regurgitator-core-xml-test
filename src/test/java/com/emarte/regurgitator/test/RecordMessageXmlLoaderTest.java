91376c9117e742c5df091d16c550fc9702064c0b
talmeym <mtalmey@gmail.com>
2016-10-31 14:07:54 +0000

    changed ordering of hashmap contents in expectations to suite new jvm version

---
+++ src/test/java/com/emarte/regurgitator/test/RecordMessageXmlLoaderTest.java
@@ -0,0 +1,20 @@
+package com.emarte.regurgitator.test;
+
+import com.emarte.regurgitator.core.RecordMessageXmlLoader;
+import org.junit.Test;
+
+public class RecordMessageXmlLoaderTest extends XmlLoaderTest {
+	public RecordMessageXmlLoaderTest() {
+		super(new RecordMessageXmlLoader());
+	}
+
+	@Test
+	public void testMin() throws Exception {
+		assertExpectation("classpath:/RecordMessage_min.xml", "com.emarte.regurgitator.core.RecordMessage:['record-message-1',null]");
+	}
+
+	@Test
+	public void testFolder() throws Exception {
+		assertExpectation("classpath:/RecordMessage_folder.xml", "com.emarte.regurgitator.core.RecordMessage:['record-message-1','/folder/folder']");
+	}
+}
