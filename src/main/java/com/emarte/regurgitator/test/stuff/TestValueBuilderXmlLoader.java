8c352da6292c5016b8d9628be527f5f6c59d0a39
talmeym <mtalmey@gmail.com>
2017-10-05 22:05:27 +0100

    sorted indentation

--- src/main/java/com/emarte/regurgitator/test/stuff/TestValueBuilderXmlLoader.java
+++ src/main/java/com/emarte/regurgitator/test/stuff/TestValueBuilderXmlLoader.java
@@ -5,8 +5,7 @@ import org.w3c.dom.Element;
 
 import java.util.Set;
 
-public class TestValueBuilderXmlLoader implements XmlLoader<ValueBuilder>
-{
+public class TestValueBuilderXmlLoader implements XmlLoader<ValueBuilder> {
     @Override
     public ValueBuilder load(Element element, Set<Object> allIds) throws RegurgitatorException {
         return new TestValueBuilder();
